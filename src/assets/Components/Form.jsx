1d5719a62af8abe283ae3968f761a4f07f187a73
RCThree <146854240+PipoCleofas@users.noreply.github.com>
2024-04-09 13:11:08 +0800

    Commit

--- src/assets/Components/Form.jsx
+++ src/assets/Components/Form.jsx
@@ -7,13 +7,14 @@ export default function Form(){
     return(
         <>
             <div>
+                <h2 id="contactUs">Ready to make your imagination a reality? </h2>
                 <form>
                     <input type="text" placeholder="Name" required/>
                     <input type="email" placeholder="Email" required/>
                     <input type="number" placeholder="Price Range" required/>
                     <input type="text" placeholder="Message" required/>
                 </form>       
-            <button>Submit</button>
+                <button>Submit</button>
             </div>
             
         </>
