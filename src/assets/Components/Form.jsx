a074c7310198f440fbf840875079ac2a176e7942
ReubenStill <157823344+ReubenStill@users.noreply.github.com>
2024-04-13 09:52:25 +0000

    CSS Changes and JS Changes by ReubenStill

--- src/assets/Components/Form.jsx
+++ src/assets/Components/Form.jsx
@@ -1,23 +1,17 @@
-
-
-export default function Form(){
-
-
-
-    return(
-        <>
-            <div>
-                <h2 id="contactUs">Ready to make your imagination a reality? </h2>
-                <form>
-                    <input type="text" placeholder="Name" required/>
-                    <input type="email" placeholder="Email" required/>
-                    <input type="number" placeholder="Price Range" required/>
-                    <input type="text" placeholder="Message" required/>
-                </form>       
-                <button>Submit</button>
-            </div>
-            
-        </>
- 
-    )
-}
+export default function Form() {
+    return (
+      <>
+        <div className="form-container">
+          <h2 id="contactUs">Ready to make your imagination a reality? </h2>
+          <form>
+            <input type="text" placeholder="Name" required />
+            <input type="email" placeholder="Email" required />
+            <input type="number" placeholder="Price Range" required />
+            <input type="text" placeholder="Message" required />
+            <button>Submit</button>
+          </form>
+        </div>
+      </>
+    );
+  }
+  
--- src/assets/Components/OurSkills.jsx
+++ src/assets/Components/OurSkills.jsx
@@ -1,25 +1,25 @@
-export default function OurSkills(){
-
-
-    return(
-        <>
-            <div>
-                <div id="skillsMessage">
-                    <h1>Skills</h1>
-                </div>
-                <div id="skills">
-                    <img src="" alt="Skill 1" />
-                    <img src="" alt="Skill 2" />
-                    <img src="" alt="Skill 3" />
-                    <img src="" alt="Skill 4" />
-                    <img src="" alt="Skill 5" />
-                    <img src="" alt="Skill 6" />
-                    <img src="" alt="Skill 7" />
-                    <img src="" alt="Skill 8" />
-                    <img src="" alt="Skill 9" />
-                </div>
-            </div>
-            
-        </>
+export default function OurSkills() {
+    return (
+      <>
+        <div>
+          <div id="skillsMessage">
+            <h1>Our Skills</h1>
+          </div>
+          <div id="skills">
+            <img src="https://images-ext-1.discordapp.net/external/PD_h0qHQEcQ3DOY1DH5BrG_eSt0uxaLpSTdxeOKRJjY/%3Ff%3Dwebp%26w%3D256/https/cdn.iconscout.com/icon/free/png-512/free-javascript-2038874-1720087.png?format=webp&quality=lossless" alt="Skill 1" />
+            <img src="https://images-ext-1.discordapp.net/external/zhkvHj_d6_nt6dLTXP6ipmQoJ5_v9JVbBIwvbXeHy3o/%3Ff%3Dwebp%26w%3D256/https/cdn.iconscout.com/icon/free/png-512/free-html5-42-1175210.png?format=webp&quality=lossless" alt="Skill 2" />
+            <img src="https://images-ext-1.discordapp.net/external/q0J8_7oB6D935OO3u6KBvZOSlECE16MWLRov5Mur6Ec/%3Ff%3Dwebp%26w%3D256/https/cdn.iconscout.com/icon/free/png-512/free-python-2-226051.png?format=webp&quality=lossless" alt="Skill 3" />
+            <img src="https://images-ext-1.discordapp.net/external/J2TWnMbZM3GbzTOVqvFO1npOYWPDp09ruzj44ol1qL0/%3Ff%3Dwebp%26w%3D256/https/cdn.iconscout.com/icon/free/png-512/free-kotlin-283155.png?format=webp&quality=lossless" alt="Skill 4" />
+            <img src="https://images-ext-1.discordapp.net/external/710eBPqpmT7IhkP5y-imSjR8-GnoWkMalDpo--dQ6UM/%3Ff%3Dwebp%26w%3D256/https/cdn.iconscout.com/icon/free/png-512/free-react-3521666-2945110.png?format=webp&quality=lossless" alt="Skill 5" />
+            <img src="https://images-ext-1.discordapp.net/external/xphdiT_wsi6GbHMFQrFkbqu-j3NyZJjD0_vpypEUn2s/%3Ff%3Dwebp%26w%3D256/https/cdn.iconscout.com/icon/free/png-512/free-c-57-1175191.png?format=webp&quality=lossless" alt="Skill 6" />
+            <img src="https://images-ext-1.discordapp.net/external/zTFISW9M55dWTgmvePDngHN4nx958bsTuVasG3llAlQ/%3Ff%3Dwebp%26w%3D256/https/cdn.iconscout.com/icon/free/png-512/free-c-2336965-1982846.png?format=webp&quality=lossless" alt="Skill 7" />
+            <img src="https://images-ext-1.discordapp.net/external/5aYx5CMUu-UXD1gQUAVNgmFlqMWjNy9oBfZjJ8LypMs/%3Ff%3Dwebp%26w%3D256/https/cdn.iconscout.com/icon/free/png-512/free-c-4-226082.png?format=webp&quality=lossless" alt="Skill 8" />
+            <img src="https://images-ext-1.discordapp.net/external/hpJxTI7SO5omapmhSOXjJ4tqjGtJO8jwz9c-lh7ASy4/%3Ff%3Dwebp%26w%3D256/https/cdn.iconscout.com/icon/free/png-512/free-typescript-3629120-3030260.png?format=webp&quality=lossless" alt="Skill 9" />
+          </div>
+        </div>
+        
+        <div style={{ marginBottom: '102px'}}> </div>
+      </>
     );
-}
+  }
+  
