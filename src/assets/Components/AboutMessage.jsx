e62b3a4a4f8a8a9bf03792b6608121f1ae84bbe9
RCThree <146854240+PipoCleofas@users.noreply.github.com>
2024-04-22 21:21:11 +0800

    Test Commit

--- src/assets/Components/AboutMessage.jsx
+++ src/assets/Components/AboutMessage.jsx
@@ -12,18 +12,20 @@ export default function AboutMessage(){
                     <p>Get ready to be captivated as we reveal the emergence of a vibrant new force in the developer world! <br /> We are pleased to announce the formation of the Nerds Studio, an unstoppable union of very intelligent individuals. <br /> These innovators have a common love of technology and invention, which puts them in a unique position to push the envelope of what is feasible in the field of development. <br /> The Nerds Studio cordially invites you to join them in celebrating the limitless possibilities of human ingenuity, the strength of teamwork, and creativity as they set off on this thrilling journey. <br /> Greetings from the start of a new era!</p>
                 </div >
 
-            <div id="firstMessage">
-                <p>Why pick <b>Nerds Studios?:</b></p>
-            </div>
+                <div id="firstMessage">
+                    <p>Why pick <b>Nerds Studios?:</b></p>
+                </div>
 
-            <div id="secondMessage">
-                <ul>
-                    <li>Join us in creating amazing digital experiences that leave a lasting impression on your audience.</li>
-                    <li>At Nerds Studio, we believe in the power of collaboration. Our team consists of talented individuals from diverse backgrounds, each bringing unique perspectives to the table.</li>
-                    <li>Our approach to every project is rooted in innovation and creativity. We thrive on challenges and continuously push the boundaries to deliver solutions that exceed expectations.</li>
-                    <li>Whether you're a startup looking to build your digital presence or an established enterprise seeking to innovate, Nerds Studio is your partner in success.</li>
-                </ul>
+                <div id="secondMessage">
+                    <ul>
+                        <li>Join us in creating amazing digital experiences that leave a lasting impression on your audience.</li>
+                        <li>At Nerds Studio, we believe in the power of collaboration. Our team consists of talented individuals from diverse backgrounds, each bringing unique perspectives to the table.</li>
+                        <li>Our approach to every project is rooted in innovation and creativity. We thrive on challenges and continuously push the boundaries to deliver solutions that exceed expectations.</li>
+                        <li>Whether you're a startup looking to build your digital presence or an established enterprise seeking to innovate, Nerds Studio is your partner in success.</li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </>
+    
     )
 }
